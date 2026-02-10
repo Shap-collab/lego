2794b434924f9db6a96eb94c6cfea99f3db86ffb
Anthony Yang <ANTHONY.YANG@EDU.DEVINCI.FR>
2026-02-10 14:15:49 +0100

    feat(deals): store the link with the highest reduction

--- client/v1/index.js
+++ client/v1/index.js
@@ -28,7 +28,9 @@ console.log(MY_FAVORITE_DEALERS[0]);
 // 🎯 TODO 1: The highest reduction
 // 0. I have 2 favorite lego sets shopping communities stored in MY_FAVORITE_DEALERS variable
 // 1. Create a new variable and assign it the link of the lego set with the highest reduction I can find on these 2 websites
+const highestReductionLegoLink = 'https://www.avenuedelabrique.com/lego-super-mario/surprise-de-super-champignon-polybag-30385/p1213';
 // 2. Log the variable
+console.log('Link to the highest reduction found:', highestReductionLegoLink);
 
 /**
  * 🧱
